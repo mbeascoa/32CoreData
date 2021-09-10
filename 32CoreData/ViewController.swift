2c0fc25d60285bfe0bb4013571b626393cb3af9d
mbeascoa <miguel.beascoabeastek.net>
2021-09-10 10:15:23 +0200

    10 septiembre

--- 32CoreData/ViewController.swift
+++ 32CoreData/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+   
+    
+    //INSERTAR REGISTRO
+    
+    if let miDelegate = UIApplication.shared.delegate as? AppDelegate {
+        
+        let miContexto = miDelegate.persistentContainer.viewContext
+        
+        let nuevoNombre = NSEntityDescription.insertNewObject(forEntityName: "Persona",
+                                                                into: miContexto)
+        nuevoNombre.setValue("Benito Floro", forKey: "Nombre")
+        
+            do {
+               try
+                miContexto.save()
+            } catch {
+               print("Error al guardar el contexto: \(error)")
+            }
+         
+                
+                //LECTURA DE DATOS
+            
+            let request : NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName:"Persona")
+           
+                if let nombresClientes = try? miContexto.fetch(request) as? [NSManagedObject] {
+               for dato in nombresClientes {
+                   print(dato.value(forKey: "Nombre")!)
+               }
+        }
 
-
+            }
+    
 }
+}
+
 
